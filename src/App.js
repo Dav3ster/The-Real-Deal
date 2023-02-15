05b0275dcb249c33e0e98d4630d10120b354adf1
unknown <davidstew87@gmail.com>
2023-02-15 01:05:16 -0500

    updated react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import HomeBar from "./components/Navbar";
 import Home from "./components/Home";
 import Applications from "./components/Applicaitons";
 import Contact from "./components/Contact"
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <Router>
        <HomeBar />
-        <Switch>
+        <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/Applications" element={<Applications />} />
           <Route exact path="/Contact" element={<Contact />} />
-        </Switch>
+        </Routes>
     </Router>
   );
 }
