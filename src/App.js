92e5438b95275718b52f712233ea2a2a7996b0e0
unknown <davidstew87@gmail.com>
2023-02-15 01:16:30 -0500

    changed index.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import HomeBar from "./components/Navbar";
 import Home from "./components/Home";
 import Applications from "./components/Applicaitons";
 import Contact from "./components/Contact"
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
